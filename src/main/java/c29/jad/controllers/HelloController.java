83fa0dec807169ad009eab60d1b13380363dc877
Mona Lisa <YOUR_EMAIL>
2024-01-30 17:21:43 +0800

    server update

---
+++ src/main/java/c29/jad/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package c29.jad.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String hello(){
+        return "Hello World";
+    }
+}
