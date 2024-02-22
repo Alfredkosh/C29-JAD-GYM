c646eaf643b0815785845fe195167b7929f23fcd
Mona Lisa <YOUR_EMAIL>
2024-02-22 12:58:17 +0800

    admin update

--- src/main/resources/public/js/loggedIn.js
+++ src/main/resources/public/js/loggedIn.js
@@ -1,47 +1,79 @@
-window.addEventListener('load', async () => {
-  await getUsername();
-  await addlogoutEventListener();
-} )
+window.addEventListener("load", async () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    const payload = parseJwt(token);
+    const userId = payload.userId;
+    const isAdmin = payload.isAdmin;
+    await getUsername(token);
+    await addlogoutEventListener();
+  } else {
+    window.location.href = "/login";
+  }
+});
+
 async function addlogoutEventListener() {
   document.querySelector("#logoutBtn").addEventListener("click", (e) => {
-    localStorage.removeItem("token")
-    window.location.reload()
-  })
-  
+    localStorage.removeItem("token");
+    window.location.reload();
+  });
 }
 
-async function getUsername() {
-// console.log(httpResponse);
+async function getUsername(token) {
+  const payload = parseJwt(token);
+  const isAdmin = payload.isAdmin;
+  // console.log(httpResponse);
   let httpResponse = await fetch("/auth/username", {
     headers: {
-      "Authorization": `Bearer ${localStorage.getItem('token')}`
-    }
+      Authorization: `Bearer ${token}`,
+    },
   });
-  let result;
+  let result1;
+  let result2;
 
-  if (httpResponse.status == 200) {
-    result = await httpResponse.json();
+  if (httpResponse.status == 200 && isAdmin == false) {
+    result1 = await httpResponse.json();
 
-      // console.log("name", result);
-  
-      document.querySelector(
-        "#username-display"
-      ).innerHTML = `<li class="nav-item text-uppercase" id="username-display">
-      <a href="/profile" class="item-anchor btn btn-outline-primary rounded-pill">Welcome ${result.data}</a>
-    </li>`;
+    console.log(result1);
+
+    // console.log("name", result);
 
     document.querySelector(
-      "#logout-area"
-    ).innerHTML = `<li class="nav-item text-uppercase" id="username-display">
-      <a href="/index" id="logoutBtn" class="item-anchor btn btn-outline-primary rounded-pill">Log out</av>
+      "#username-display"
+    ).innerHTML = `<li class="nav-item text-uppercase" >
+      <a href="/profile" class="item-anchor btn btn-outline-primary rounded-pill">Welcome ${result1.data}</a>
+    </li>`;
+  } else if (httpResponse.status == 200 && isAdmin == true) {
+    result2 = await httpResponse.json();
+
+    document.querySelector(
+      "#username-display"
+    ).innerHTML = `<li class="nav-item text-uppercase" >
+      <a href="/admin" class="item-anchor btn btn-outline-primary rounded-pill">Welcome ${result2.data}</a>
     </li>`;
 
-    document.querySelector("#username-display .item-anchor").addEventListener("click", () => {
-      // Redirect to the desired page
-      window.location.href = "/profile";
-    });
   } else {
-    result = await httpResponse.json();
+    await httpResponse.json();
   }
+  document.querySelector(
+    "#logout-area"
+  ).innerHTML = `<li class="nav-item text-uppercase" >
+    <a href="/index" id="logoutBtn" class="item-anchor btn btn-outline-primary rounded-pill">Log out</av>
+  </li>`;
+
 }
 
+function parseJwt(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
