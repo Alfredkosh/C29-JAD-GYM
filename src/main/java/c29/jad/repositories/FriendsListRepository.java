70a508ea67af672f7ec5c17c1e543b8b6e7cc58d
Mona Lisa <YOUR_EMAIL>
2024-02-23 15:22:51 +0800

    html update

--- src/main/java/c29/jad/repositories/FriendsListRepository.java
+++ src/main/java/c29/jad/repositories/FriendsListRepository.java
@@ -11,15 +11,10 @@ import java.util.List;
 public interface FriendsListRepository extends JpaRepository<FriendsModel, Integer> {
     FriendsModel findUsernameById(Integer id);
 
+}
+
 //    boolean existsByUsername(String username);
-//
-//import c29.jad.models.FriendsModel;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
-//
-//import java.util.List;
-//
+
 //public interface FriendsListRepository extends JpaRepository<FriendsModel, Integer> {
 //    FriendsModel findUsernameById(Integer id);
 //
